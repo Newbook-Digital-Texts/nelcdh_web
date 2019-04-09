0657bb9e7091e70ee41f798de274fb3f2dc990f1
Darren <37914187+darren-w-huang@users.noreply.github.com>
2019-04-09 15:04:48 -0700

    Add files via upload

--- local.js
+++ local.js
@@ -3,19 +3,16 @@ $(document).ready(function(){
       'font-size' : "3vw",
         'opacity': '1',
     },3000)});
-
-$(document).ready(function(){
-    $("h1").animate({ 
-      'font-size' : "3vw",
-        'opacity': '1',
-    },3000);
-});
-let array = ['lightyellow','lightgreen','lightblue']
+let r,b,p,y,g;
+// Unpack values rom array into variables
+[r,b,p,y,g] = ['lightcoral','lightblue','lightpink','lightyellow','lightgreen'];
+let array = [r,g,g,g,r,g,b,r,p,b,b,g,p,p,g,r,r,g,b,p,p,p,p,y,b,p,r,p,b,r,r,r,p,b,p,y,b];
+let counter = 0;
 let containers = document.querySelectorAll(".tl-timemarker .tl-timemarker-content-container");
 for (x of containers) {
-	//console.log(array[Math.floor(Math.random()*3)]);
-	//console.log(x)
-	x.style.backgroundColor = array[Math.floor(Math.random()*3)];
+	x.style.backgroundColor = array[counter++];
 }
+// Checking code
+console.log(document.querySelectorAll(".tl-timegroup-message"));
 console.log(containers);
 console.log("hello");
