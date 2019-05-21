117f7f49e2a6690bd5b76bef991b1ef4ffbdf7a6
Darren <37914187+darren-w-huang@users.noreply.github.com>
2019-05-20 22:48:14 -0700

    Initial

---
+++ boyo-child/footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Boyo
+ */
+
+get_sidebar( 'bottom' );
+boyo_css_loader( 'footer' );
+?>
+
+	</div><!-- #content -->
+	<?php get_sidebar( 'footer' ); ?>
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				
+				printf(('<a href="http://www.washington.edu/online/privacy//">Privacy|</a>' ));
+				printf(('<a href = "https://depts.washington.edu/newbook/staff/staff.html">Terms|</a>' ));
+				printf(('<a href="http://www.washington.edu/online/terms/">STAFF LOGIN</a>' ));
+
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
